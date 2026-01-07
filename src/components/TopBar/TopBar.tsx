bbec4cb6569ece5c3ac1bbf564469fe5113bfb36
creativecoderjoy <154061032+creativecoderjoy@users.noreply.github.com>
2026-01-07 21:03:54 +0100

    added dashboard side bar and top bar

---
+++ src/components/TopBar/TopBar.tsx
@@ -0,0 +1,23 @@
+import { Search } from "lucide-react";
+import styles from "./TopBar.module.css";
+
+export default function TopBar() {
+  return (
+    <div className={styles.topBar}>
+      <div className={styles.left}>
+        <div className={styles.searchWrapper}>
+          <Search size={18} className={styles.searchIcon} />
+          <input
+            type="text"
+            placeholder="Search logs (Cmd + K)"
+            className={styles.search}
+          />
+        </div>
+      </div>
+
+      <div className={styles.right}>
+        <h5>Oct 26 2023</h5>
+      </div>
+    </div>
+  );
+}
