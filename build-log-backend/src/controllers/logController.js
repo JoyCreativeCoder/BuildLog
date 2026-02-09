94f94a802af8c4fe488fbfc3c480097dea95825f
creativecoderjoy <154061032+creativecoderjoy@users.noreply.github.com>
2026-02-09 22:21:42 +0100

    setup routes and end-points for delete and update

--- build-log-backend/src/controllers/logController.js
+++ build-log-backend/src/controllers/logController.js
@@ -25,3 +25,39 @@ export const getLogs = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
+
+export const deleteLog = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const deletedLog = await Log.findByIdAndDelete(id);
+    if (!deletedLog) {
+      return res.status(400).json({ message: "log not found" });
+    }
+    res.status(200).json({ message: "Log deleted successfully " });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+export const updateLog = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const updateData = req.body;
+
+    if (Object.keys(updateData).length === 0) {
+      return res.status(400).json({ message: "No data provided to update" });
+    }
+
+    const updatedLog = await Log.findByIdAndUpdate(id, updateData, {
+      new: true,
+    });
+
+    if (!updatedLog) {
+      return res.status(404).json({ message: "Log not found" });
+    }
+
+    res.status(200).json({ message: "Log updated successfully", updatedLog });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
